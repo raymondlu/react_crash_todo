84d934b04407953ec8ce822213c510b09a3a00aa
Raymond Lu <raymondlu1105@qq.com>
2019-06-15 09:57:32 +0800

    Refactor App

--- src/App.js
+++ src/App.js
@@ -19,31 +19,6 @@ class App extends Component {
       .then(res => this.setState({ todos: res.data }))
   }
 
-  // Toggle Complete
-  markComplete = (id) => {
-    this.setState({ todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed
-      }
-      return todo;
-    }) });
-  }
-
-  // Delete Todo
-  delTodo = (id) => {
-    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }));
-  }
-
-  // Add Todo
-  addTodo = (title) => {
-    axios.post('https://jsonplaceholder.typicode.com/todos', {
-      title,
-      completed: false
-    })
-      .then(res => {this.setState({ todos: [...this.state.todos, res.data] });});
-  }
-
   render() {
     return (
       <Router>
@@ -52,8 +27,8 @@ class App extends Component {
             <Header />
             <Route exact path="/" render={props => (
               <React.Fragment>
-                <AddTodo addTodo={this.addTodo} />
-                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+                <AddTodo />
+                <Todos todos={this.state.todos} />
               </React.Fragment>
             )} />
             <Route path="/about" component={About} />
